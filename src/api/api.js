a841a8beb8d9f3b4faa03c58e118beb712b7af7b
sao333167 <sao333167@gmail.com>
2026-02-25 09:26:23 +0700

    deploy update

--- src/api/api.js
+++ src/api/api.js
@@ -1,9 +1,21 @@
 import axios from "axios";
 const local = 'http://localhost:5000'
+const production = 'http://localhost:6000'
+
+let api_url = ''
+let mode = 'pro'
+
+if (mode === 'pro') {
+    api_url = production
+} else {
+    api_url = local
+}
+
 
 const api = axios.create({
-    baseURL: `${local}/api`
+    baseURL : `${api_url}/api`
 })
 
 
-export default api;
+
+export default api
